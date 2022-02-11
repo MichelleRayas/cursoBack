f13b053a96939bef0dcfc634d2a1f5f2f5cf868c
Michelle Rayas <michelle.rayas98@gmail.com>
2022-02-11 16:58:09 -0600

    update

--- demo/src/main/java/controllers/productosController.java
+++ demo/src/main/java/controllers/productosController.java
@@ -3,6 +3,7 @@ package controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.productos;
 
+import io.swagger.annotations.Api;
 import services.productoService;
 
+@CrossOrigin(origins = "http://localhost:8090")
 @RestController
 @RequestMapping("/api")
+@Api(value = "Producto")
 public class productosController {
 	private productoService categService;
 	
